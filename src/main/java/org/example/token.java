fca21ef821131fd675d96a5f4a67981ce7e7ad2d
lastsummernight <lastsummerdayosu@gmail.com>
2024-09-23 16:52:04 +0500

    Echobot, reversed line

--- src/main/java/org/example/token.java
+++ src/main/java/org/example/token.java
@@ -2,5 +2,5 @@ package org.example;
 
 public class token {
     //Установить значение при работе
-    static String token = "7755790713:AAHKXwkSiCZWnZRzD317QPSIyQr7kXI7Sbs";
+    static String token = "";
 }
