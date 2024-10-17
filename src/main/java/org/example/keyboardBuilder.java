f503312ba3252694f4216008d888d92015ac3197
alfadefender <serejenk4.m@yandex.ru>
2024-10-17 20:37:37 +0500

    making keyboard on key word

--- src/main/java/org/example/keyboardBuilder.java
+++ src/main/java/org/example/keyboardBuilder.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 
 import java.util.ArrayList;
@@ -9,73 +10,37 @@ import java.util.List;
 
 public class keyboardBuilder {
 
-    public ReplyKeyboardMarkup buildKeyboard(List<String> array) {
+    public ReplyKeyboardMarkup buildKeyboard(String key) {
 
-        int rows = 2;
-        int columns = 2;
 
         List<KeyboardRow> keyboard = new ArrayList<>();
-        for (int countRow = 0; countRow != rows; countRow++) {
-            KeyboardRow keyboardRow = new KeyboardRow();
-            for (int countCol = 0; countCol != columns; countCol++) {
-                keyboardRow.add(array.get(countRow * rows + countCol));
-            }
-            keyboard.add(keyboardRow);
+        KeyboardRow keyboardRow = new KeyboardRow();
+
+        switch (key) {
+            case "sex":
+                keyboardRow.add("Парень");
+                keyboardRow.add("Девушка");
+                keyboard.add(keyboardRow);
+                break;
+
+            case "main_menu":
+                keyboardRow.add("Поиск");
+                keyboardRow.add("Моя анкета");
+                keyboardRow.add("Сообщения");
+                keyboard.add(keyboardRow);
+                break;
+
+            case "like_dislike":
+                keyboardRow.add("♥");
+                keyboardRow.add("\uD83D\uDC94"); // разбитое сердечко
+                keyboard.add(keyboardRow);
+                break;
         }
 
         ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(keyboard);
         replyKeyboardMarkup.setSelective(true);
         replyKeyboardMarkup.setResizeKeyboard(true);
-        //replyKeyboardMarkup.setOneTimeKeyboard(false);
-
-        return replyKeyboardMarkup;
-    }
-
-    public ReplyKeyboardMarkup buildKeyboard() {
-
-        List<String> ListOfButtons = Arrays.asList("Предложение ничьи", "Сдаться", "NW", "N", "NE", "W", "C", "E", "SW", "S", "SE");
-        List<KeyboardRow> keyboard = new ArrayList<>();
-
-        for (int countRow = -1; countRow != 3; countRow++) {
-            KeyboardRow keyboardRow = new KeyboardRow();
-
-            if (countRow == -1) {
-                keyboardRow.add(ListOfButtons.get(0));
-                keyboardRow.add(ListOfButtons.get(1));
-            }
-
-            else {
-                for (int countCol = 0; countCol != 3; countCol++) {
-                    keyboardRow.add(ListOfButtons.get(countRow * 3 + countCol + 2));
-                }
-            }
-
-            keyboard.add(keyboardRow);
-
-        }
-
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(keyboard);
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
-        //replyKeyboardMarkup.setOneTimeKeyboard(false);
-
-        return replyKeyboardMarkup;
-    }
-
-    public ReplyKeyboardMarkup buildKeyboard(int rows, int columns) {
-
-        List<KeyboardRow> keyboard = new ArrayList<>();
-        for (int countRow = 0; countRow != rows; countRow++) {
-            KeyboardRow keyboardRow = new KeyboardRow();
-            for (int countCol = 0; countCol != columns; countCol++) {
-                keyboardRow.add(Integer.toString(countRow * 3 + countCol + 1));
-            }
-            keyboard.add(keyboardRow);
-        }
-
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(keyboard);
-        replyKeyboardMarkup.setSelective(true);
-        replyKeyboardMarkup.setResizeKeyboard(true);
+        replyKeyboardMarkup.setOneTimeKeyboard(true);
 
         return replyKeyboardMarkup;
     }
