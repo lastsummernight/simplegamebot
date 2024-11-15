12415c99372a524d4340b220301378aafaf748cb
alfadefender <serejenk4.m@yandex.ru>
2024-11-15 15:23:09 +0500

    added logging messages of users

---
+++ src/main/java/com/github/simplegamebot/Debugger.java
@@ -0,0 +1,20 @@
+package com.github.simplegamebot;
+
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+
+public class Debugger {
+
+    public static void printProfile(Profile profile) throws UnsupportedEncodingException {
+        PrintStream printStream = new PrintStream(System.out, true, "utf-8");
+
+        printStream.println("{ id = " + profile.getChatId());
+        printStream.println("   Name = " + profile.getUsername());
+        printStream.println("   Age = " + profile.getAge());
+        printStream.println("   City = " + profile.getCity());
+        printStream.println("   Gender = " + profile.getGender());
+        printStream.println("   Info = " + profile.getInfo());
+        printStream.println("}");
+    }
+
+}
