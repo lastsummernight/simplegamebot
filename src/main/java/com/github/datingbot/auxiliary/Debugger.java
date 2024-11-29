b38940417f0a49ce6937c0f1483b0d1e497b1c95
alfadefender <serejenk4.m@yandex.ru>
2024-11-29 13:29:37 +0500

    some exceptions for simplify getting unsupposed situations

--- src/main/java/com/github/datingbot/auxiliary/Debugger.java
+++ src/main/java/com/github/datingbot/auxiliary/Debugger.java
@@ -7,9 +7,18 @@ import java.io.UnsupportedEncodingException;
 
 public class Debugger {
 
-    public static void printProfile(Profile profile) throws UnsupportedEncodingException {
-        PrintStream printStream = new PrintStream(System.out, true, "utf-8");
+    private static PrintStream printStream;
 
+    public static void setUp() {
+        try {
+            printStream = new PrintStream(System.out, true, "utf-8");
+        }
+        catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void printProfile(Profile profile) {
         printStream.println("{ id = " + profile.getChatId());
         printStream.println("   Name = " + profile.getUsername());
         printStream.println("   Age = " + profile.getAge());
@@ -17,6 +26,14 @@ public class Debugger {
         printStream.println("   Gender = " + profile.getGender());
         printStream.println("   Info = " + profile.getInfo());
         printStream.println("}");
+    }
+
+    public static void printException(MyException e) {
+        printStream.println(e.returnTraceback());
+    }
+
+    public static void printException(CustomException e) {
+        printStream.println(e.getTraceback());
     }
 
 }
