d958686f32227e375bb7c3375f49da37345a335b
alfadefender <serejenk4.m@yandex.ru>
2024-12-13 15:23:56 +0500

    enum for hobbies

---
+++ src/main/java/com/github/datingbot/auxiliary/Hobbies.java
@@ -0,0 +1,45 @@
+package com.github.datingbot.auxiliary;
+
+import jdk.dynalink.linker.LinkerServices;
+
+import java.util.Arrays;
+import java.util.List;
+
+public enum Hobbies {
+
+    LITERATURE_HOBBY(0,"Литература"),
+    DANCE_HOBBY(1, "Танцы"),
+    VIDEOGAMES_HOBBY(2, "Видеоигры"),
+    SCIENCE_HOBBY(3, "Наука"),
+    SPORT_HOBBY(4, "Спорт"),
+    MUSIC_HOBBY(5, "Музыка"),
+    COOKING_HOBBY(6, "Готовка"),
+    TRAVELLING_HOBBY(7, "Путешествия"),
+    ART_HOBBY(8, "Рисование");
+
+    private String specificValue;
+    private String title;
+
+    private Hobbies(int num, String name) {
+        specificValue = Integer.toString(num);
+        title = name;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getSpecificValue() {
+        return specificValue;
+    }
+
+    public static Hobbies getHobbyBySpecificValue(String key) {
+        try {
+            return List.of(Hobbies.values()).get(StringFunctions.isNum(key));
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
